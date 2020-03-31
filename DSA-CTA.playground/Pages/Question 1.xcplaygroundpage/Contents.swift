fd94c937117ae5e0500d989d3cf0bfb3523adba1
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-03-30 20:02:07 -0400

    completed question 1

--- DSA-CTA.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
+++ DSA-CTA.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
@@ -7,10 +7,60 @@ import Foundation
 class Node<T> {
     var next: Node?
     let val: T
-    init(val: T) {
+    init(_ val: T) {
         self.val = val
     }
 }
+
+extension Node: CustomStringConvertible {
+    var description: String {
+        guard let next = next else {
+            return "\(val) -> nil"
+        }
+        return "\(val) -> \(next)"
+    }
+}
+
+class LinkedList<T> {
+    var head: Node<T>?
+    var tail: Node<T>?
+    
+    public var isEmpty: Bool {
+        print("empty list")
+        return head == nil
+    }
+    
+    
+    
+    public func append(_ value: T) {
+        let newNode = Node(value)
+        
+        guard let lastNode = tail else {
+            head = newNode
+            tail = head
+            return
+        }
+        
+        lastNode.next = newNode
+        tail = newNode
+        
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    var description: String {
+        guard let head = head else {
+            return "empty list"
+        }
+        return "-> \(head)"
+    }
+}
+
+let list = LinkedList<String>()
+list.append("oscar")
+list.append("ivan")
+list.append("alex")
+print(list)
 
 // Print format:
 //```
