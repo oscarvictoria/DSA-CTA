da761463f5c757bf38f3c6605413cb3ab7e2bbd6
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-04-04 00:25:01 -0400

    updated question 6

--- DSA-CTA.playground/Pages/Question 6.xcplaygroundpage/Contents.swift
+++ DSA-CTA.playground/Pages/Question 6.xcplaygroundpage/Contents.swift
@@ -8,3 +8,14 @@ import Foundation
 // Your solution should be **recursive**. Your function must be pure (cannot use global variables)
 // Sample Input [5, 2, 9, 11]
 // Sample Output: 27
+
+func sum(_ array: [Int])-> Int {
+    guard array.count > 0 else { return 0 }
+    let arraySlice = array.dropFirst()
+    let answer = array[0] + sum(Array(arraySlice))
+    
+   return answer
+}
+
+var numbers = [1,2,3]
+print(sum(numbers))
